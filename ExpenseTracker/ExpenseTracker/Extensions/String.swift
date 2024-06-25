8ef36c6c24efffec6d9acea1f0d3a4cabb120c38
LASKAV <mksidoops@gmail.com>
2024-06-25 23:07:17 +0200

    feat: extension String

--- ExpenseTracker/ExpenseTracker/Extensions/String.swift
+++ ExpenseTracker/ExpenseTracker/Extensions/String.swift
@@ -2,5 +2,9 @@ import Foundation
 
 
 extension String {
-    
+    func dateParsed() -> Date {
+        guard let parsedDate = DateFormatter.allNumericUSA.date(from: self) else { return Date()}
+        
+        return parsedDate
+    }
 }
